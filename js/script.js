6a78d50502cdb37761989871fc6b2f76e84cd8d6
Andrey Cheshko <andreycheshko@gmail.com>
2024-12-09 20:24:33 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -12,10 +12,9 @@ let burger = document.querySelector('.burger');
 let headerControl = document.querySelector('.header__control');
 let nav = document.querySelector('.nav');
 let navItems = nav.querySelectorAll('.nav__link');
-let header = document.querySelector('.header');
 let overlay = document.querySelector('.overlay');
 
-burger.addEventListener('click', () => {
+const toggleMenu = () => {
   document.body.classList.toggle('stop-scroll');
   burger.classList.toggle('burger--active');
   overlay.classList.toggle('overlay--visible');
@@ -30,89 +29,63 @@ burger.addEventListener('click', () => {
     }, 600);
   } else {
     headerControl.classList.add('header__control--initial');
-    headerControl.classList.add('header__control--visible');
+    setTimeout(() => {
+      headerControl.classList.add('header__control--visible');
+    }, 20); // небольшая задержка, чтобы transition применился
   }
-});
+};
 
-overlay.addEventListener('click', () => {
+const closeMenu = () => {
   document.body.classList.remove('stop-scroll');
   burger.classList.remove('burger--active');
   headerControl.classList.remove('header__control--visible');
   overlay.classList.remove('overlay--visible');
   burger.setAttribute('aria-expanded', 'false');
-});
 
-navItems.forEach((el) => {
-  el.addEventListener('click', () => {
-    document.body.classList.remove('stop-scroll');
-    burger.classList.remove('burger--active');
-    headerControl.classList.remove('header__control--visible');
-    overlay.classList.remove('overlay--visible');
+  setTimeout(() => {
+    headerControl.classList.remove('header__control--initial');
+  }, 600);
+};
 
-    burger.setAttribute('aria-expanded', false);
-  });
-});
+burger.addEventListener('click', toggleMenu);
+overlay.addEventListener('click', closeMenu);
+
+navItems.forEach((el) => el.addEventListener('click', closeMenu));
 
 window.addEventListener('resize', () => {
   if (window.innerWidth > 767) {
     headerControl.style.transition = 'none';
-    headerControl.classList.remove('header__control--visible');
-    headerControl.classList.remove('header__control--initial');
-    overlay.classList.remove('overlay--visible');
-    burger.classList.remove('burger--active');
-    document.body.classList.remove('stop-scroll');
-    burger.setAttribute('aria-expanded', 'false');
+    closeMenu();
     setTimeout(() => {
-      nav.style.transition = '';
+      headerControl.style.transition = '';
     }, 0);
   }
 });
 
-// Карточки в слайдер на адаптиве
-window.addEventListener('DOMContentLoaded', () => {
-  const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
-    let swiper;
-
-    breakpoint = window.matchMedia(breakpoint);
-
-    const enableSwiper = function (className, settings) {
-      swiper = new Swiper(className, settings);
-
-      if (callback) {
-        callback(swiper);
-      }
-    };
-
-    const checker = function () {
-      if (breakpoint.matches) {
-        return enableSwiper(swiperClass, swiperSettings);
-      } else {
-        if (swiper !== undefined) swiper.destroy(true, true);
-        return;
-      }
-    };
-
-    breakpoint.addEventListener('change', checker);
-    checker();
-  };
-
-  const someFunc = (instance) => {
-    if (instance) {
-      instance.on('slideChange', function (e) {
-        console.log('*** mySwiper.activeIndex', instance.activeIndex);
-      });
-    }
-  };
-
-  resizableSwiper('(max-width: 1023px)', '.slider', {
-    loop: true,
-    spaceBetween: 0,
-    slidesPerView: 2,
-    freeMode: true,
-    breakpoints: {
-      900: {
-        slidesPerView: 3,
-      },
+//Swiper
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
     },
-  });
+    767: {
+      slidesPerView: 2.5,
+    },
+    575: {
+      slidesPerView: 1.5,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+  },
 });
