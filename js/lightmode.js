8d7ec350df097a053d477de8c637cf49c8c375e0
Andrey Cheshko <andreycheshko@gmail.com>
2024-12-09 23:28:20 +0300

    add mobile version for eng

--- js/lightmode.js
+++ js/lightmode.js
@@ -1,19 +1,19 @@
-let lightmode = localStorage.getItem('lightmode');
-const themeSwitch = document.querySelector('.theme-switch');
+// let lightmode = localStorage.getItem('lightmode');
+// const themeSwitch = document.querySelector('.theme-switch');
 
-const enableLightMode = () => {
-  document.body.classList.add('lightmode');
-  localStorage.setItem('lightmode', 'active');
-};
+// const enableLightMode = () => {
+//   document.body.classList.add('lightmode');
+//   localStorage.setItem('lightmode', 'active');
+// };
 
-const disabeLightMode = () => {
-  document.body.classList.remove('lightmode');
-  localStorage.setItem('lightmode', null);
-};
+// const disabeLightMode = () => {
+//   document.body.classList.remove('lightmode');
+//   localStorage.setItem('lightmode', null);
+// };
 
-if (lightmode === 'active') enableLightMode();
+// if (lightmode === 'active') enableLightMode();
 
-themeSwitch.addEventListener('click', () => {
-  lightmode = localStorage.getItem('lightmode');
-  lightmode !== 'active' ? enableLightMode() : disabeLightMode();
-});
+// themeSwitch.addEventListener('click', () => {
+//   lightmode = localStorage.getItem('lightmode');
+//   lightmode !== 'active' ? enableLightMode() : disabeLightMode();
+// });
--- js/script.js
+++ js/script.js
@@ -5,6 +5,29 @@ langRu.addEventListener('click', function (event) {
   if (this.classList.contains('lang--inactive')) {
     event.preventDefault();
   }
+  closeMenu();
+});
+
+// Тёмный-светлый режим
+let lightmode = localStorage.getItem('lightmode');
+const themeSwitch = document.querySelector('.theme-switch');
+
+const enableLightMode = () => {
+  document.body.classList.add('lightmode');
+  localStorage.setItem('lightmode', 'active');
+};
+
+const disabeLightMode = () => {
+  document.body.classList.remove('lightmode');
+  localStorage.setItem('lightmode', null);
+};
+
+if (lightmode === 'active') enableLightMode();
+
+themeSwitch.addEventListener('click', () => {
+  lightmode = localStorage.getItem('lightmode');
+  lightmode !== 'active' ? enableLightMode() : disabeLightMode();
+  closeMenu();
 });
 
 // Мобильное меню
